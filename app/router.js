19f27f484e2fd235c251012bb7ba16d6cfc336a6
craigvantonder <craig@compulutions.co.za>
2016-03-08 05:09:28 +0200

    Added code for video 4

--- app/router.js
+++ app/router.js
@@ -6,8 +6,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('homepage');
-  this.route('books');
+  this.route('books', function() {
+    this.route('book', {
+      path: ':book_id'
+    }); // books/3
+  });
 });
 
 export default Router;
